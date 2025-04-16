1222e436bff71d97146dda710a2407aa6f968a50
Evgenii Ansel <ave1122@yandex.ru>
2025-04-16 21:27:36 +0300

    Заготовка (сервер не работает)

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        preset: 'ts-jest',
+        collectCoverage: true,
+        coverageDirectory: 'coverage',
+        coverageProvider: 'v8'
+      }
+    ]
+  }
+};
+
+export default config;
